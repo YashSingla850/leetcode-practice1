b3f11b3a40d545221b7306ac4b28fe98e1cbe4c8
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-08-02 10:12:03 +0530

    Time: 1 ms (99.98%), Space: 44.8 MB (94.37%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -4,23 +4,22 @@ class Solution {
         int[][]dp = new int[n+1][m+1];
         for(int []d:dp)Arrays.fill(d , -1);
         int[][]dir = {{-1,0},{0,-1}};
-        return helper(grid , dp  , n-1 , m-1);
+        return helper(grid , dp  ,dir,  n-1 , m-1);
     }
-    public int helper(int[][]grid , int[][]dp , int n ,int m){
+    public int helper(int[][]grid , int[][]dp ,int[][]dir ,int n ,int m){
         if(n==0&&m==0){
             return dp[n][m] = grid[n][m];
         }
-         if(n<0 || m<0 || n>=grid.length || m>=grid[0].length){
-             return (int)1e9;
-         }
-        if(dp[n][m]!=-1)return dp[n][m];
-        
-        
-        int min = Integer.MAX_VALUE;
-        int a = helper(grid , dp , n-1,m);
-        int b = helper(grid , dp , n , m-1);
-           min = Math.min(min , Math.min(a,b));
-    return dp[n][m]=min+grid[n][m];
+         if(dp[n][m]!=-1)return dp[n][m];
+              int min = Integer.MAX_VALUE;
+           for(int d =0;d<dir.length;d++){
+               int r = n+dir[d][0];
+               int c = m+dir[d][1];
+               if(r>=0 && c>=0 && r<grid.length && c< grid[0].length){
+                   min = Math.min(min,helper(grid , dp , dir, r, c));
+               }
+           }
+            return dp[n][m]  = min+grid[n][m];
     }
   
 }
