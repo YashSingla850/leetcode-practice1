699e9f0d27966e38820aaebeaa6703d2bc20a435
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-08-08 12:42:02 +0530

    Time: 52 ms (46.66%), Space: 54.8 MB (25.50%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -10,17 +10,51 @@ class Solution {
         int temp =  helper(n , m , s , t, dp);
             return n-temp;
     }
-    public int helper(int n , int m , String s , String t , int[][]dp){
-        if(n==0 || m==0){
-            return dp[n][m] =0;
-        }
-        if(dp[n][m]!=-1)return dp[n][m];
-       
-            if(s.charAt(n-1)==t.charAt(m-1)){
-                return dp[n][m] = 1+ helper(n-1 , m-1 , s, t, dp);
+    public int helper(int N , int M , String s , String t , int[][]dp){
+        for(int n =0;n<=N;n++){
+            for(int m =0;m<=M;m++){
+               if(n==0 || m==0){
+                     dp[n][m] =0;
+                   continue;
+                }
+                if(n>0 && m>0){
+                 if(s.charAt(n-1)==t.charAt(m-1)){
+                  dp[n][m] = 1+ dp[n-1][m-1];
             }else{
-                return dp[n][m]  = Math.max(helper(n-1 , m , s, t, dp) , helper(n ,m-1 , s, t, dp));
-            }
-
+                 dp[n][m]  = Math.max (dp[n-1][m],dp[n][m-1]);
+                }
+              }
+           }
+        }
+       
+       return dp[N][M];
     }
 }
+
+
+// class Solution {
+//     public int minInsertions(String s) {
+//         StringBuilder sb = new StringBuilder(s);
+//         sb.reverse();
+//         String t = sb.toString();
+//         int n = s.length();
+//         int m  = t.length();
+//         int dp[][] = new int[n+1][m+1];
+//         for(int d[]:dp)Arrays.fill(d,-1);
+//         int temp =  helper(n , m , s , t, dp);
+//             return n-temp;
+//     }
+//     public int helper(int n , int m , String s , String t , int[][]dp){
+//         if(n==0 || m==0){
+//             return dp[n][m] =0;
+//         }
+//         if(dp[n][m]!=-1)return dp[n][m];
+       
+//             if(s.charAt(n-1)==t.charAt(m-1)){
+//                 return dp[n][m] = 1+ helper(n-1 , m-1 , s, t, dp);
+//             }else{
+//                 return dp[n][m]  = Math.max(helper(n-1 , m , s, t, dp) , helper(n ,m-1 , s, t, dp));
+//             }
+
+//     }
+// }
