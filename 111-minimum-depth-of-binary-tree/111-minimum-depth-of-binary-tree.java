66ceaf0d98be84bac5f3722fd84905ea4e3a1066
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-09-24 21:18:54 +0530

    Time: 1 ms (98.07%), Space: 61.9 MB (96.61%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.java
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.java
@@ -14,20 +14,43 @@
  * }
  */
 class Solution {
-    int min = (int)1e9;
     public int minDepth(TreeNode root) {
-     min = (int)1e9;
+        Queue<TreeNode>q = new ArrayDeque<>();
         if(root==null)return 0;
-                helper(root,1);
-        return min;
-    }
-    public void helper(TreeNode node , int count){
-        if(node==null)return;
-        if(node.left==null && node.right==null){
-            min =Math.min(count , min); 
-            return ;
+        q.add(root);
+        int count =1;
+        while(q.size()>0){
+            int n = q.size();
+            for(int i  = 0 ; i<n ;i++){
+                TreeNode temp = q.remove();
+                if(temp.left==null && temp.right==null){
+                    return count;
+                }
+                if(temp.left!=null)q.add(temp.left);
+                if(temp.right!=null)q.add(temp.right);
+            }
+           count++;
         }
-        helper(node.left , count+1);
-        helper(node.right , count+1);
+        return 0;
     }
 }
+
+
+// class Solution {
+//     int min = (int)1e9;
+//     public int minDepth(TreeNode root) {
+//      min = (int)1e9;
+//         if(root==null)return 0;
+//                 helper(root,1);
+//         return min;
+//     }
+//     public void helper(TreeNode node , int count){
+//         if(node==null)return;
+//         if(node.left==null && node.right==null){
+//             min =Math.min(count , min); 
+//             return ;
+//         }
+//         helper(node.left , count+1);
+//         helper(node.right , count+1);
+//     }
+// }
