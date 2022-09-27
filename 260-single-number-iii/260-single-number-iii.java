268eeb7a1ca4ff8106291a3b444a53a565ff1f49
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-09-27 12:41:04 +0530

    Time: 1 ms (100.00%), Space: 42.1 MB (98.14%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.java
+++ 260-single-number-iii/260-single-number-iii.java
@@ -1,54 +1,48 @@
-class Solution {
-    public int[] singleNumber(int[] nums) {
-        int xor=0; // this will provide us the xor of the two distict numbers.
-           for(int n:nums){
-            xor=xor^n;
-               }
-
-        int set_bit=0; // this finds the last set bit of xor.
-        
-        for(int i=0;i<32;i++){
-            if((xor&(1<<i))!=0){
-                set_bit=1<<i;
-            }
-        }
-        
-        int[] ans=new int[2];// to divide nums into two groups which will eventually contain the required         distinct numbers.
-        
-        for(int n:nums){
-            if((n&set_bit)==0){
-                ans[0]^=n;
-            }else{
-                ans[1]^=n;
-            }
-        }
-        
-        return ans;
-        
-    }
-	}
-
 // class Solution {
 //     public int[] singleNumber(int[] nums) {
-//         int xor = 0;
-//          for(int i=0 ; i<nums.length;i++){
-//              xor = xor^nums[i];
-//          }
-//         int set_bit =0 ;
-//         for(int i =0 ; i<33 ; i++){
-//             if((xor&(i<<i))!=0){
-//                 set_bit = 1<<i;
+//         int xor=0; // this will provide us the xor of the two distict numbers.
+//            for(int n:nums){
+//             xor=xor^n;
+//                }
+//         int set_bit=0; // this finds the last set bit of xor
+//         for(int i=0;i<32;i++){
+//             if((xor&(1<<i))!=0){
+//                 set_bit=1<<i;
 //             }
 //         }
-        
-//         int ans [] = new int [2];
-//         for(int num : nums){
-//             if((num&set_bit)==0){
-//                 ans[0] = ans[0]^num;
+//         int[] ans=new int[2];// to divide nums into two groups which will eventually contain 
+//         for(int n:nums){
+//             if((n&set_bit)==0){
+//                 ans[0]^=n;
 //             }else{
-//                 ans[1] = ans[1]^num;
+//                 ans[1]^=n;
 //             }
 //         }
 //         return ans;
 //     }
-// }
+// 	}
+
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor = 0;
+         for(int i=0 ; i<nums.length;i++){
+             xor = xor^nums[i];
+         }
+        int set_bit =0 ;
+        for(int i =0 ; i<32 ; i++){
+            if((xor&(1<<i))!=0){
+                set_bit = 1<<i;
+            }
+        }
+        
+        int ans [] = new int [2];
+        for(int num : nums){
+            if((num&set_bit)==0){
+                ans[0]^=num;
+            }else{
+                ans[1]^=num;
+            }
+        }
+        return ans;
+    }
+}
