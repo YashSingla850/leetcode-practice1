7b53e9d0cdc0828f1c66b9683a8a436762ecf5d5
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-09-27 12:57:17 +0530

    Time: 44 ms (6.53%), Space: 60.7 MB (5.15%) - LeetHub

---
+++ 645-set-mismatch/645-set-mismatch.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+     HashMap<Integer,Integer>map = new HashMap<>();
+        int max = 0 ;
+        int min = (int)1e9;
+        for(int i =0 ; i<nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+            max = Math.max(max ,nums[i]);
+            min = Math.min(min , nums[i]);
+        }
+        int ans [] = new int[2];
+        for(int i = 1; i<=max ;i++){
+             if(map.containsKey(i)==true){
+                 int freq = map.get(i);
+                 if(freq==2){
+                     ans[0] = i;
+                 }
+             }else{
+                 ans[1] = i;
+             }
+        }
+        if(ans[1]==0){
+            ans[1] = max+1;
+        }
+        return ans;
+    }
+}
