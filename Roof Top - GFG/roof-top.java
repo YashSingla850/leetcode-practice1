44b974c64e8e6c0ef61ab39cb4216b2377dd0f90
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-11-19 16:05:50 +0530

    Added solution - LeetHub

---
+++ Roof Top - GFG/roof-top.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class gfg
+{
+    
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(read.readLine());
+            
+            int arr[] = new int[n];
+            
+            String st[] = read.readLine().trim().split("\\s+");
+            
+            for(int i = 0; i < n; i++)
+              arr[(int)i] = Integer.parseInt(st[(int)i]);
+              
+            
+            System.out.println(new Solution().maxStep(arr, n));
+        }
+    }
+    
+    
+}
+    
+    
+
+// } Driver Code Ends
+
+
+
+class Solution
+{
+    //Function to find maximum number of consecutive steps 
+    //to gain an increase in altitude with each step.
+    static int maxStep(int arr[], int N)
+    {
+        // Your code here
+        int max =0;
+        int count =0;
+        for(int i=0 ; i<N-1 ; i++){
+            if(arr[i+1]>arr[i]){
+                count++;
+                max = Math.max(count ,max);
+            }else if(arr[i+1]<=arr[i]){
+                count =0;
+            }
+        }
+        return max;
+    }
+    
+}
