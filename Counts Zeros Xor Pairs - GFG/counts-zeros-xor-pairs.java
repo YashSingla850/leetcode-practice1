3205f9525f3383039352ea04fdc7693ffb790166
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-12-06 09:37:27 +0530

    Added solution - LeetHub

---
+++ Counts Zeros Xor Pairs - GFG/counts-zeros-xor-pairs.java
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    
+		    Complete obj = new Complete();
+		    long res = obj.calculate(arr, sizeOfArray);
+		    System.out.println(res);
+		}
+	}
+}
+
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static long calculate (int arr[], int n) {
+        HashMap<Integer ,Integer>map = new HashMap<>();
+        long count =0;
+        for(int val:arr){
+            map.put(val, map.getOrDefault(val,0)+1);
+        }
+        
+        for(int key:map.keySet()){
+            int freq = map.get(key);
+            count+=((freq-1)*freq)/2;
+        }
+        return count;
+    }
+    
+    
+}
+
+
