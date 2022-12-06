8bbdc3332da6ffd04208147bc646ad38d4ba0278
YashSingla850 <87891888+YashSingla850@users.noreply.github.com>
2022-12-06 14:14:48 +0530

    Added solution - LeetHub

---
+++ Find Transition Point - GFG/find-transition-point.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Sorted_Array {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            GfG obj = new GfG();
+            System.out.println(obj.transitionPoint(arr, n));
+            T--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class GfG {
+    int transitionPoint(int arr[], int n) {
+        int lo = 0 , hi = n-1;
+        int ans =(int)1e9;
+         while(lo<=hi){
+             int mid = lo+(hi-lo)/2;
+             if(arr[mid]<1){
+                 lo = mid+1;
+             }else if(arr[mid]>=1){
+                 ans = Math.min(ans, mid);
+                  hi = mid-1;
+             }
+         }
+         if(ans == 1e9)return -1;
+         return ans;
+    }
+}
